ef8b0ffad3f962cb836a2db80111b6edd84d031a
Karthik Talluri <karthik1391@Karthiks-MacBook-Pro-2.local>
2023-11-04 19:48:00 -0400

    Full working UI

--- hackathonEducation.py
+++ hackathonEducation.py
@@ -1,7 +1,8 @@
 import google.generativeai as palm
 API_KEY = 'AIzaSyDKbnLMFZ3I1H2iXcBMsYpe31IIu4BM4aM'
 palm.configure(api_key = API_KEY)
-from flask import Flask, jsonify, request, render_template
+import re
+from flask import Flask, jsonify, request, render_template, render_template_string
 
 app = Flask(__name__)
 model_id = "models/text-bison-001"
@@ -24,17 +25,21 @@ def findcourses(jobroles):
   courses = []
   for i in jobroles:
     prompt = f'Can you briefly explain what courses do I need to take in college to become {i}? Just list out top 5 courses names as a starred list only. Do not add any title'
-    completion=palm.generate_text(
-    model=model_id,
-    prompt=prompt,
-    temperature=0.99,
-    max_output_tokens=800,
+    completion = palm.generate_text(
+      model=model_id,
+      prompt=prompt,
+      temperature=0.99,
+      max_output_tokens=800,
     )
     result = completion.result
-    result_mod = result.replace('*', '')
-    result_mod = result_mod.replace(f'Top 5 College Courses for {i}s', '')
-    courses.append(result_mod)
-  courses = [x.strip(' ') for x in courses]
+    # Assuming the result comes in a list format, separated by new lines
+    # First, we remove the asterisk and split by new lines
+    courses_list = result.replace('*', '').split('\n')
+    # Then we strip whitespace and filter out empty strings
+    courses_list = [course.strip() for course in courses_list if course.strip()]
+    # Join the courses with a comma
+    courses_joined = ', '.join(courses_list)
+    courses.append(courses_joined)
   return courses
 
 def findcolleges(jobroles):
@@ -93,6 +98,17 @@ def findjobroles(interests):
         "colleges": findcolleges(jobroles),
         "salary": findsalaryrange(jobroles),
     }
+
+def clean_text(text):
+    # Remove \n and \r characters
+    text = text.replace('\n', ' ').replace('\r', ' ')
+    # Remove special characters you consider unnecessary
+    # Add or remove characters in the regex as needed
+    text = re.sub(r'[;*#]', '', text)
+    # Additional stripping to remove potential double spaces
+    text = re.sub(' +', ' ', text).strip()
+    return text
+
 @app.route('/')
 def index():
     return render_template('index.html')  # Your input form page
@@ -101,13 +117,16 @@ def index():
 def get_job_roles():
     interests = request.form['interests']
     job_roles_data = findjobroles(interests)
-    output = ""
-    for key, values in job_roles_data.items():
-        output += key + "; "
-        for value in values:
-            output += value + " "
-        output += " "
-    return output
+    
+    # Generate list of lists for the table
+    job_roles_data['jobroles'] = [clean_text(role) for role in job_roles_data['jobroles']]
+    job_roles_data['JD'] = [clean_text(jd) for jd in job_roles_data['JD']]
+    job_roles_data['courses'] = [clean_text(course) for course in job_roles_data['courses']]
+    job_roles_data['colleges'] = [clean_text(college) for college in job_roles_data['colleges']]
+    job_roles_data['salary'] = [clean_text(salary) for salary in job_roles_data['salary']]
+    
+    # Here we should return a JSON response because the frontend is expecting it
+    return jsonify(job_roles_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
